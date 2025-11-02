2c9c379e0a18f9ed4e8048c395892087aae150a8
Xingbo Wang <xbw@fb.com>
2025-11-01 19:52:35 -0700

    rename

--- src.mk
+++ src.mk
@@ -599,7 +599,7 @@ TEST_MAIN_SOURCES =                                                     \
   table/sst_file_reader_test.cc                                         \
   table/table_test.cc                                                   \
   table/user_defined_block_test.cc                                      \
-  table/user_defined_block_test2.cc                                     \
+  table/rexdb_test.cc				                                    \
   table/block_fetcher_test.cc                                           \
   test_util/testutil_test.cc                                            \
   util/compression_test.cc                                            \
